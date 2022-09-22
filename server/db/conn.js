09ac44f3ddbca9775088bc686343b9f1eae4b0db
emilie <emilie.dannenberg@gmail.com>
2022-09-22 17:19:39 -0500

    add server files

---
+++ server/db/conn.js
@@ -0,0 +1,26 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://paddleroutes:iluv2paddle@cluster0.o3atuqr.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  console.log('i connected yeah');
+  client.close(); 
+});
+
+
+//
+//[longitude, latitude, elevation]
+
+//collection paddles
+// {
+//   // id: 1,
+//   // name: 'Bay St. Louis Loop',
+//   // start: {lat: 30.2988043,  lng: -89.4077829},
+//   // tags: [
+//   //   'Mississipi',
+//   //   'Medium',
+//   //   'Open water'
+//   // ]
+// },
+
+
+//
