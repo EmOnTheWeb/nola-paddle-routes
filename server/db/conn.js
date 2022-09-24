8edd1efa1dabb01fb7922022cba99408f67f47c9
emilie <emilie.dannenberg@gmail.com>
2022-09-24 14:11:48 -0500

    recommit this file with env variables

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,10 +1,30 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://paddleroutes:iluv2paddle@cluster0.o3atuqr.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.o3atuqr.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
   console.log('i connected yeah');
-  client.close(); 
+  client.close();
 });
+
+
+//
+//[longitude, latitude, elevation]
+
+//collection paddles
+// {
+//   // id: 1,
+//   // name: 'Bay St. Louis Loop',
+//   // start: {lat: 30.2988043,  lng: -89.4077829},
+//   // tags: [
+//   //   'Mississipi',
+//   //   'Medium',
+//   //   'Open water'
+//   // ]
+// },
+
+
+//
+
 
 
 //
